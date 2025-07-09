030864e84cf9bc391f7b14be8397ac779e95ce8c
crinuleiroz <usanimere@outlook.com>
2025-07-09 04:52:06 -0700

    Update Zelda64 Audiobin Generator.py

--- Zelda64 Audiobin Generator.py
+++ Zelda64 Audiobin Generator.py
@@ -317,9 +317,9 @@ if __name__ == '__main__':
     match_data: tuple[str, ...] = None
 
     for endianness, games in GAME_SIGNATURES.items():
-      for key, info in games.items():
+      for game_key, info in games.items():
         if info['signature'] in rom_header:
-          match_data = (endianness, key, info)
+          match_data = (endianness, game_key, info)
           break
       if match_data:
         break
